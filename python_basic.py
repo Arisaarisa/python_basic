7e40ae5a4d880789fd6f3d9df8221e28218621ae
arisa <arisankochira10@gmail.com>
2018-11-20 21:28:28 +0900

    A7 0が入ってしまうのでどうしたらいいかわからない

--- python_basic.py
+++ python_basic.py
@@ -3,21 +3,19 @@
 # users = ['Bob', 'Tom','Ken']
 #
 #
-# int_numbers = 1
-# while int_numbers <= 5:
-#     print(int_numbers)
-#     int_numbers += 1
+int_numbers = 1
+while int_numbers <= 5:
+    print(int_numbers)
+    int_numbers += 1
+
+# numbers = 1
+# while numbers <= 10:
+#     print(numbers)
+#     numbers += 1
 
 
-
-numbers = 1
-while numbers <= 10:
-    print(numbers)
-    numbers += 1
-
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-for odd_number in numbers:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for odd_number in numbers:#odd_number = 1,3,5,7,9
     if odd_number % 2 == 0:
         continue
 
@@ -26,18 +24,33 @@ for odd_number in numbers:
 
 
 
+count = 1
+
+for even_number in range(0,10,2):
+    print(even_number*2)
+    count += 1
+
+#print(5:)
+# for even_number in numbers:
+#     if even_number % 3 == 0:
+#
+#         continue
+#     print(even_number * 2)
+#     if even_number == 8:
+#         break
+
+# even_number = [2,4,6,8]
+
 
 # def kazuma_info(self, name,family,age):
 #     self.name = kazuma_info('Kazuma')
 #     self.family = kazuma_info('Takahashi')
 #
-kazuma_info = ['Kazuma','Takahashi',35]
+kazuma_info = ['Kazuma', 'Takahashi', 35]
 for kazuma in kazuma_info:
     print("Name:" + kazuma_info[0] + kazuma_info[1] + "age:" + str(kazuma_info[2]))
     break
 
-
 menbers = ['Kazuma', 'Makoto', 'Ohira']
 print(menbers[0])
 print(menbers[2])
-
