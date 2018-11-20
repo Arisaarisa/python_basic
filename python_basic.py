cc7aabb51ed345f4813fb713d1c93afdab8c6dec
arisa <arisankochira10@gmail.com>
2018-11-20 19:38:44 +0900

    A5

---
+++ python_basic.py
@@ -0,0 +1,22 @@
+# class User:
+#     def __init__(self):
+users = ['Bob', 'Tom','Ken']
+#
+#
+int_numbers = [1,2,3,4,5]
+
+
+# def kazuma_info(self, name,family,age):
+#     self.name = kazuma_info('Kazuma')
+#     self.family = kazuma_info('Takahashi')
+#
+kazuma_info = ['Kazuma','Takahashi',35]
+for kazuma in kazuma_info:
+    print("Name:" + kazuma_info[0] + kazuma_info[1] + "age:" + str(kazuma_info[2]))
+    break
+
+
+menbers = ['Kazuma', 'Makoto', 'Ohira']
+print(menbers[0])
+print(menbers[2])
+
