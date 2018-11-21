a1dbad238ec41a164ed0967b69476c02d3ea55bd
arisa <arisankochira10@gmail.com>
2018-11-21 13:08:22 +0900

    A10

--- python_basic.py
+++ python_basic.py
@@ -1,17 +1,14 @@
-# class User:
-#     def __init__(self):
-# users = ['Bob', 'Tom','Ken']
-#
-#
 int_numbers = 1
 while int_numbers <= 5:
     print(int_numbers)
     int_numbers += 1
 
-# numbers = 1
-# while numbers <= 10:
-#     print(numbers)
-#     numbers += 1
+
+
+
+
+
+
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -30,22 +27,12 @@ for even_number in range(0,10,2):
     print(even_number*2)
     count += 1
 
-#print(5:)
-# for even_number in numbers:
-#     if even_number % 3 == 0:
-#
-#         continue
-#     print(even_number * 2)
-#     if even_number == 8:
-#         break
-
-# even_number = [2,4,6,8]
 
 
-# def kazuma_info(self, name,family,age):
-#     self.name = kazuma_info('Kazuma')
-#     self.family = kazuma_info('Takahashi')
-#
+
+
+
+
 kazuma_info = ['Kazuma', 'Takahashi', 35]
 for kazuma in kazuma_info:
     print("Name:" + kazuma_info[0] + kazuma_info[1] + "age:" + str(kazuma_info[2]))
@@ -67,3 +54,19 @@ print(kazuma_info["first_name"])
 print(kazuma_info["family_name"])
 print(kazuma_info["age"])
 
+
+
+
+
+
+
+import random
+
+
+
+def dice():
+    num = [1,2,3,4,5,6,]
+    print(str(random.choice(num)) + "の目が出ました")
+
+
+dice()
